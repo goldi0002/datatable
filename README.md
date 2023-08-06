de309dbeab98aa1ead025fcacaaa8bf99197b58a
Gaurav Thakur <gauravt@channel-fusion.com>
2023-08-06 10:53:09 +0530

    nothing

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # ExcelExport
- 
+
+
+# Examples{
+
+const headers = ['Name', 'Age', 'Country', 'City'];    
+const data = [
+  ['John Doe', 30, 'USA', 'New York'],
+  ['Jane Smith', 25, 'Canada', 'Toronto'],
+  ['Michael Johnson', 40, 'UK', 'London']
+];
+
+
+
+exportToExcel('Sheet1', headers, data, {
+  columnOrder: ['Name', 'City', 'Country', 'Age'],
+  customFilename: 'my_exported_data'
+});
+
+
+
+const data = [
+  { person: { name: 'John', age: 30 }, country: 'USA' },
+  { person: { name: 'Jane', age: 25 }, country: 'Canada' },
+];
+
+exportToExcel('Sheet1', ['Name', 'Age', 'Country'], data, {
+  nestedDataDelimiter: 'person.name',
+  transformCellValue: val => val.toUpperCase(),
+  customFilename: 'nested_data_export'
+});
+
+
+((((((( escapeSpecialChars(value, escapeChar))))))))
+This function takes two arguments:
+value: The input string that needs to be escaped.
+escapeChar: The escape character used in the CSV format. In most cases, this is a double quote (").
+The function uses a regular expression to replace any occurrences of the escape character, newline (\n), or carriage return (\r) in the input string with the escape character repeated twice. This effectively escapes these characters according to the CSV format.
+For example, if you have a cell value like "Hello, world!", using escapeSpecialChars would ensure that it's properly formatted for CSV export:
+
+# }
+
+
--- excelScript.js
+++ excelScript.js
@@ -1,68 +1,112 @@
 function exportToExcel(sheetName, headers, data, options = {}) {
-  const {
-    delimiter = ',',
-    includeHeaders = true,
-    customFilename = '',
-    timeStamp = true,
-    footer = [],
-    fileExtension = 'csv', // Added option for file extension
-  } = options;
-
-  const csvRows = [];
-  let timestamp = '';
-  let sheetname = sheetName ? sheetName : generateRandomText(6);
+    try {
+      const {
+        delimiter = ',',
+        includeHeaders = true,
+        customFilename = '',
+        title = '',
+        timeStamp = true,
+        footer = [],
+        separatorRow = false,
+        excludeColumns = [],
+        fileExtension = 'csv',
+        charset = 'utf-8',
+        lineEnding = '\n',
+        transformCellValue = val => val,
+        autoFormat = true,
+        conditionalFormatting = null,
+        nestedDataDelimiter = '.', // Use dot (.) as the delimiter
+        decimalSeparator = '.', // Customize decimal separator
+        thousandsSeparator = ',', // Customize thousands separator
+      } = options;
   
-  if (includeHeaders) {
-    const formattedHeaders = headers.map(header => `"${header}"`).join(delimiter);
-    csvRows.push(formattedHeaders);
-  }
-  
-  data.forEach(row => {
-    const formattedRow = row.map(cell => {
-      if (typeof cell === 'string') {
-        return `"${cell}"`;
-      } else if (typeof cell === 'number') {
-        return cell.toString();
-      } else {
-        return '';
+      if (!headers || !Array.isArray(headers) || !data || !Array.isArray(data) || headers.length === 0 || data.length === 0) {
+        throw new Error('Invalid headers or data provided.');
       }
+  
+      const columnOrder = headers.filter(header => !excludeColumns.includes(header));
+      const formattedHeaders = includeHeaders ? columnOrder.map(header => `"${header}"`).join(delimiter) : '';
+      const formattedRows = data.map(row =>
+        formatRow(row, headers, columnOrder, delimiter, nestedDataDelimiter, transformCellValue, decimalSeparator, thousandsSeparator)
+      );
+  
+      const csvContent = [
+        title,
+        formattedHeaders,
+        ...(separatorRow ? [''.padEnd(formattedHeaders.length, '-')] : []),
+        ...formattedRows,
+        ...footer.map(cell => `"${cell}"`)
+      ].join(lineEnding);
+  
+      const baseFilename = customFilename || sheetName || generateRandomText(6);
+      const timeStampPart = timeStamp ? `_${new Date().toISOString().replace(/[-:.]/g, '')}` : '';
+      const fileName = `${baseFilename}${timeStampPart}.${fileExtension}`;
+      const blob = new Blob([csvContent], { type: `text/csv;charset=${charset};` });
+      const url = URL.createObjectURL(blob);
+      downloadFile(url, fileName);
+    } catch (error) {
+      console.error('An error occurred during CSV export:', error);
+      throw error; // Rethrow the error to propagate it
+    }
+  }  
+  function formatHeaders(columnOrder, delimiter) {
+    return columnOrder.map(header => `"${header}"`).join(delimiter);
+  }
+  function formatRow(row, headers, columnOrder, delimiter, nestedDataDelimiter, transformCellValue, decimalSeparator, thousandsSeparator) {
+    return columnOrder.map((column) => {
+      const columnIndex = headers.indexOf(column);
+      if (columnIndex !== -1) {
+        const cellValue = resolveNestedValue(row, column, nestedDataDelimiter);
+        const formattedValue = formatCellValue(cellValue, decimalSeparator, thousandsSeparator);
+        return `"${escapeSpecialChars(formattedValue)}"`;
+      }
+      return '';
     }).join(delimiter);
-    
-    csvRows.push(formattedRow);
-  });
-
-  if (footer.length > 0) {
-    const footerRow = footer.map(cell => `"${cell}"`).join(delimiter);
-    csvRows.push(footerRow);
   }
-  
-  if (timeStamp) {
-    timestamp = new Date().toISOString().replace(/[-:.]/g, ''); // Get a timestamp in a format suitable for a file name
+  function resolveNestedValue(obj, path, nestedDataDelimiter) {
+    const keys = path.split(nestedDataDelimiter);
+    return keys.reduce((result, key) => (result && result[key] !== undefined) ? result[key] : '', obj);
+  }  
+  function escapeSpecialChars(value) {
+    let EscapecedChars=value.replace(/"/g, '""');
+    return EscapecedChars;
   }
-  
-  const baseFilename = customFilename || sheetname;
-  const fileName = `${baseFilename}${timeStamp ? '_' + timestamp : ''}.${fileExtension}`; // Use the specified file extension
-  const csvContent = csvRows.join('\n');
-  
-  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-  const url = URL.createObjectURL(blob);
-  
-  const link = document.createElement('a');
-  link.setAttribute('href', url);
-  link.setAttribute('download', fileName);
-  link.style.display = 'none';
-  
-  document.body.appendChild(link);
-  link.click();
-  
-  document.body.removeChild(link);
-}
-function generateRandomText(length) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      result += characters.charAt(randomIndex);
+  function downloadFile(url, fileName) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
-  return result;
-}
+  function generateRandomText(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+    }
+    return result;
+  }
+  function formatCellValue(value, decimalSeparator, thousandsSeparator) {
+    if (typeof value === 'number') {
+      return value.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2, decimalSeparator, thousandsSeparator });
+    } else if (value instanceof Date) {
+      return value.toLocaleDateString(); // Format dates as localized strings
+    } else {
+      return value.toString();
+    }
+  }
+function applyConditionalFormatting(value, conditions) {
+    if (!conditions) {
+      return value;
+    }
+  
+    for (const condition of conditions) {
+      if (condition.test(value)) {
+        return condition.format(value);
+      }
+    }
+    return value;
+  }
